dabd2ec65074033085f14189bd8c722f0dc1bb5d
Leandro Mendez <lmendez89@gmail.com>
2020-04-19 21:20:41 -0300

    Create folders from movie names
    Create folders from movie names

---
+++ movie2folder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for file in *.mkv *.mp4 *.wmv *.avi; do
+        if [ -f "$file" ] # does file exist?
+        then
+                dir=$(echo "${file%.*}") # extract filename from filename.extention
+                if [ "$dir" ] # check if string found
+                then
+                        mkdir -p "$dir"  # create dir
+                        mv "$file" "$dir"     # move file into new dir
+                else
+                        echo "INCORRECT FILE FORMAT: \""$file"\"" # print error if file format is unexpected
+                fi
+        fi
+done
